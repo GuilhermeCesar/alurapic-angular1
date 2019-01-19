1bf27200aa49df9714f83f219d898c50d4ccb918
guilherme <gui11_3@hotmail.com>
2019-01-19 08:36:15 -0200

    Add factory

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,50 +1,41 @@
 angular.module('alurapic')
-	.controller('FotoController', function($scope, $http,$routeParams) {
+	.controller('FotoController', function($scope,$routeParams,recursoFoto) {
 
 		$scope.foto = {};
 		$scope.mensagem = '';
 
-		// var recursoFoto = $resource('v1/fotos/:fotoId',null,{
-		// 	update:{
-		// 		method:'PUT'
-		// 	}
-		// });
-
-
 		if($routeParams.fotoId) {
-			// $http.get('/v1/fotos/' + $routeParams.fotoId)
-			// .success(function(foto) {
-			// 	$scope.foto = foto;
-			// })
-			// .error(function(erro) {
-			// 	console.log(erro);
-			// 	$scope.mensagem = 'Não foi possível obter a foto'
-			// });
+			recursoFoto.get(
+				{fotoId:$routeParams.fotoId},
+				(foto)=>$scope.foto = foto,
+				(erro) =>{
+					console.log(erro);
+					$scope.mensagem = 'Não foi possível obter a foto'
+				}
+			);
 		}
 
-		if($routeParams.fotoId) {
-			// recursoFoto.update({fotoId: $scope.foto._id},$scope.foto,
-			// 	function (result) {
-			// 		$scope.mensagem = 'Foto alterada com sucesso';
-			// 	},function (erro) {
-			// 		console.log(erro);
-			// 		$scope.mensagem = 'Não foi possível cadastrar a foto';
-			// 	}
-			// );
-		// } else {
-		// 	$http.post('/v1/fotos', $scope.foto)
-		// 		.success(function() {
-		// 			$scope.foto = {};
-		// 			$scope.mensagem = 'Foto cadastrada com sucesso';
-		// 		})
-		// 		.error(function(erro) {
-		//
-		// 		})
-		}
+
 
 		$scope.submeter = function() {
 
 			if ($scope.formulario.$valid) {
+				if($routeParams.fotoId) {
+					recursoFoto.update({fotoId: $scope.foto._id},$scope.foto,
+						() => {
+							$scope.mensagem = 'Foto alterada com sucesso';
+						},(erro) => {
+							console.log(erro);
+							$scope.mensagem = 'Não foi possível cadastrar a foto';
+						}
+					);
+					} else {
+						recursoFoto.save($scope.foto,
+							() => {
+								$scope.foto = {};
+								$scope.mensagem = 'Foto cadastrada com sucesso';
+							},(error) => console.error(error));
+				}
 			}
 		};
 	});
